93572c67ae3f460f70410f9b5d284bc1244bd0bc
khanhhd0411 <kienkakax1@gmail.com>
2020-03-26 09:15:28 +0700

    2 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from "./app-routing.module";
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
@@ -8,7 +9,7 @@ import { ManagerComponent } from './manager/manager.component';
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, AppRoutingModule ],
   declarations: [ AppComponent, HelloComponent, ManagerComponent, HomeComponent ],
   bootstrap:    [ AppComponent ]
 })
